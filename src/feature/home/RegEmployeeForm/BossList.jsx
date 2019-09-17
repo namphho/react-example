d8d6878a9a100ad7edc6b1c98b1db8036a2dfaf5
Nam Pham <nam.phho@gmail.com>
2019-09-17 13:07:14 +0700

    remove unused component

--- src/feature/home/RegEmployeeForm/BossList.jsx
+++ src/feature/home/RegEmployeeForm/BossList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { List, Button, Image } from "semantic-ui-react";
+import { List} from "semantic-ui-react";
 import BossItem from "./BossItem";
 
 const data = [
